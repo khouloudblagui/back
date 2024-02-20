b9d63539319c0cd22aca2f98aba8d9c920d7e669
Amal-Belguith <amalbelguith00@gmail.com>
2024-02-20 18:36:31 +0100

    Add setting microservice

---
+++ src/main/java/com/example/Setting/SettingApplication.java
@@ -0,0 +1,14 @@
+package com.example.Setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingApplication.class, args);
+	}
+
+}
